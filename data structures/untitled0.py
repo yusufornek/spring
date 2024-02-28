2adf09447789d8317498cf307dc21171d6dffc0b
Test 2 <test2@istbtc.local>
2024-02-28 15:55:33 +0300

    push

---
+++ data structures/untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+ 
+print("Hello World")
+"""
+def toplam(sayi1, sayi2):
+    sonuc = sayi1 + sayi2
+    return (sonuc)
+"""
---
+++ oop/variableResulation/variableResulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+
+namespace variableResulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a number : ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            kareAl(sayi);
+        }
+        public static void kareAl(int sayi)
+        {
+            int sonuc;
+            sonuc = sayi * sayi;
+            goruntule(sonuc);
+        }
+        public static void goruntule(int number)
+        {
+
+            Console.WriteLine(number);
+        }
+        
+        
+        
+    }
+}
