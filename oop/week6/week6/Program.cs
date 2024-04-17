0af0d153e054908d6142f28e810d67e12c00635f
Yusuf <43914317+yusufornek@users.noreply.github.com>
2024-04-17 16:13:54 +0300

    Add files via upload

---
+++ oop/week6/week6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace week6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(arithmetic.kare(5.6),2)); //İki basamağa yuvarladık Math.Round fonksiyonu ile birlikte.
+            Console.WriteLine(Math.Floor(arithmetic.kare(5.6))); //Aşağı yuvarlama
+            Console.WriteLine(Math.Ceiling(arithmetic.kare(5.6))); //Yukarı yuvarlama
+            Console.WriteLine(arithmetic.kare(5));
+            Console.WriteLine(arithmetic.varsayilandegermethodu(8)); //Parametre girersem gönderdiğim parametre çalışır.
+            Console.WriteLine(arithmetic.varsayilandegermethodu()); //Parametre girmezsem varsayılan parametre değeri çalışır.
+        }   
+         /*IEE1254 FORMATINA BAK!*/
+    }
+}
---
+++ oop/week6/week6/arithmetic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week6
+{
+    internal class arithmetic
+    {
+        public static int kare(int a)
+        {
+
+            return a * a;
+        }
+
+        public static double kare(double a)
+        {
+            return a * a;
+        }
+        public static int varsayilandegermethodu(int a = 7)
+        {
+            return a * a;
+        }
+        public static int reflimethod(ref int a)
+        {
+            return a * a;
+        }
+        public static int outlumethod(out int a)
+        {
+            a = 0; //Out parametresi içeride mutlaka tekrar tanımlanmalıdır.
+            return a * a;
+        }
+
+
+    }
+}
