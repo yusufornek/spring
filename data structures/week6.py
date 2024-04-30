7bd1355bf7c10b75de94465a6c866471f57740c0
Test 2 <test2@istbtc.local>
2024-04-30 14:05:02 +0300

    push

---
+++ data structures/week6.py
@@ -0,0 +1,6 @@
+
+#Key value pairs is mean Hash Table and in Python it is called as Dictionary.
+#The features of hash tables are: these are mutable, unordered, indexed, and no duplicate values are allowed.
+#The features of Dictionary are: these are mutable, unordered, indexed, and no duplicate values are allowed. (Key value pairs)
+
+                                                                        
